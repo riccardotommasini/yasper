2c38160adbe11904234324eadd79feb5b3ac630b
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-08-24 14:48:28 +0200

    docs

--- yasper-core/src/main/java/it/polimi/yasper/core/engine/features/StreamDeletionFeature.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.yasper.core.engine.features;
-
-import it.polimi.yasper.core.stream.Stream;
-
-public interface StreamDeletionFeature<S extends Stream> {
-
-    void unregister(S s);
-
-}
